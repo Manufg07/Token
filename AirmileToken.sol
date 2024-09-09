16aef0cb1546a8fd18310e4d9dc487dbf0c9c1e4
Manufg07 <manunarayananfg@gmail.com>
2024-09-09 10:23:09 +0530

    Token

---
+++ AirmileToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract AirmileT is ERC20 {
+
+     address public admin;
+
+    constructor() ERC20("AirmileT", "AT") {
+        _mint(msg.sender, 100000000000000000000);
+        admin = msg.sender;
+    }
+
+        modifier onlyAdmin (){
+        require(msg.sender == admin,"Unautherized access");
+        _;
+    }
+
+    function safeMint(address to, uint value) public onlyAdmin {
+        _mint(to, value);
+    }
+
+}
